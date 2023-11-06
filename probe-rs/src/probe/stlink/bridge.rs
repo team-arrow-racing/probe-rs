2ad4806744d15caae9d4786b2fd98b1d938c6e45
Liam Kinne <liam@kinne.io>
2023-11-06 19:21:54 +1000

    make constants public

--- probe-rs/src/probe/stlink/bridge.rs
+++ probe-rs/src/probe/stlink/bridge.rs
@@ -1,36 +1,36 @@
 
 
 pub mod commands {
-    const CLOSE: u8 = 0x01;
-    const GET_RWCMD_STATUS: u8 = 0x02;
-    const GET_CLOCK: u8 = 0x03;
+    pub const CLOSE: u8 = 0x01;
+    pub const GET_RWCMD_STATUS: u8 = 0x02;
+    pub const GET_CLOCK: u8 = 0x03;
 
     // SPI
-    const SPI_INIT: u8 = 0x20;
-    const SPI_WRITE: u8 = 0x21;
-    const SPI_READ: u8 = 0x22;
-    const SPI_CS: u8 = 0x23;
+    pub const SPI_INIT: u8 = 0x20;
+    pub const SPI_WRITE: u8 = 0x21;
+    pub const SPI_READ: u8 = 0x22;
+    pub const SPI_CS: u8 = 0x23;
 
     // I2C
-    const I2C_INIT: u8 = 0x30;
-    const I2C_WRITE: u8 = 0x31;
-    const I2C_READ: u8 = 0x32;
-    const I2C_READ_NO_WAIT: u8 = 0x33; // new in V3B3
-    const I2C_GET_READ_DATA: u8 = 0x34; // new in V3B3
+    pub const I2C_INIT: u8 = 0x30;
+    pub const I2C_WRITE: u8 = 0x31;
+    pub const I2C_READ: u8 = 0x32;
+    pub const I2C_READ_NO_WAIT: u8 = 0x33; // new in V3B3
+    pub const I2C_GET_READ_DATA: u8 = 0x34; // new in V3B3
 
     // CAN
-    const CAN_INIT: u8 = 0x40;
-    const CAN_WRITE_MSG: u8 = 0x41;
-    const CAN_INIT_FILTER: u8 = 0x43;
-    const CAN_START_MSG_RX: u8 = 0x44;
-    const CAN_STOP_MSG_RX: u8 = 0x45;
-    const CAN_GET_RXMSG_NB: u8 = 0x46;
-    const CAN_GET_RXMSG: u8 = 0x47;
+    pub const CAN_INIT: u8 = 0x40;
+    pub const CAN_WRITE_MSG: u8 = 0x41;
+    pub const CAN_INIT_FILTER: u8 = 0x43;
+    pub const CAN_START_MSG_RX: u8 = 0x44;
+    pub const CAN_STOP_MSG_RX: u8 = 0x45;
+    pub const CAN_GET_RXMSG_NB: u8 = 0x46;
+    pub const CAN_GET_RXMSG: u8 = 0x47;
 
     // GPIO
-    const GPIO_INIT: u8 = 0x60;
-    const GPIO_SET_RESET: u8 = 0x61;
-    const GPIO_READ: u8 = 0x62;
+    pub const GPIO_INIT: u8 = 0x60;
+    pub const GPIO_SET_RESET: u8 = 0x61;
+    pub const GPIO_READ: u8 = 0x62;
 }
 
 /// STLink bridge status codes and errors.
